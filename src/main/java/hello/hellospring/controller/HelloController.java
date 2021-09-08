7b514216020f01af9ce34ad834977d0ee09d8836
jojogreen91 <iawbg13@gmail.com>
2021-09-08 14:18:12 +0900

    MVC 와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,12 @@ public class HelloController {
 
         // 타임리프 탬플릿 엔진을 설정 해놨기 때문에 resources/templates 의 hello.html 파일을 화면으로 출력한다.(response)
         return "Hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc (@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-templates";
     }
 }
