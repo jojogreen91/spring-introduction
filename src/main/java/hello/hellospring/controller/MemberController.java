9ec5733ea733702bf6f7f254bbbbea8e30639881
jojogreen91 <iawbg13@gmail.com>
2021-09-11 19:30:25 +0900

    멤버 등록 기능 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +16,24 @@ public class MemberController {
     @Autowired // Service 에 대한 의존성 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // 새로운 멤버 정보를 받기 위해서 createMembersForm 화면을 요청 & 출력
+    @GetMapping("/members/new")
+    public String createForm () {
+        return "members/createMembersForm";
+    }
+
+    // createMembersForm 화면에서 입력된 정보가, 매개변수로 받는 MemberForm 객체의 매핑된 필드에 대입 & 생성(post) 돠고
+    // 생성된 memberForm 이 매개변수로 들어오고 create 메서드가 실행됨.
+    @PostMapping("/members/new")
+    public String create (MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        // 등록 종료 후 다시 home 화면으로 복귀
+        return "redirect:/";
     }
 }
